82416055890e5f8cf84d46c84d916419afc20361
NicoHC185 <nikohidalgo.castillo@gmail.com>
2025-02-15 16:58:28 -0300

    :sparkles: added endpoint health-check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
-import { ProductsModule } from './products/products.module';
-import { OrdersModule } from './orders/orders.module';
-import { NastModule } from './transports/nast.module';
-import { PaymentsModule } from './payments/payments.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthCheckModule } from './health-check/health-check.module';
+import { Module } from '@nestjs/common';
+import { NastModule } from './transports/nast.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule, OrdersModule, NastModule, PaymentsModule, AuthModule],
+  imports: [ProductsModule, OrdersModule, NastModule, PaymentsModule, AuthModule, HealthCheckModule],
   controllers: [],
   providers: [],
 })
